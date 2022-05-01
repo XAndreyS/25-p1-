c930ab27c2f91a2dd6383b79fe6269b6d83a5afc
XAndreyS <archer.wipt@gmail.com>
2022-05-01 23:38:45 +0500

    version 2.0

--- tests/test_card_list_my_pest.py
+++ tests/test_card_list_my_pest.py
@@ -1,6 +1,11 @@
 import pytest
 import time
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture(autouse=True)
 def testing():
@@ -11,6 +16,41 @@ def testing():
     yield
 
     pytest.driver.quit()
+
+def test_card_pets():
+    """Проверка карточек притомцев, в списке: все притомцы /all_pets"""
+
+    # Вводим email
+    pytest.driver.find_element_by_id('email').send_keys('alone.test1@mail.ru')
+    # Вводим пароль
+    pytest.driver.find_element_by_id('pass').send_keys('1234')
+    # Нажимаем на кнопку входа в аккаунт
+    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+    """Добавляем неявное ожидание, загрузки всех карточек питомцев на странице /all_pets"""
+    try:
+        pytest.driver.implicitly_wait(10)
+        card_element_wait = pytest.driver.find_element_by_css_selector(".card-deck")  # неявное ожидание
+        card_element_wait
+    except TimeoutException:
+        print ("время ожидания загрузки карточек ВЫШЛО!")
+        pytest.driver.quit()
+
+    images = pytest.driver.find_elements_by_css_selector('.card-deck .card-img-top')
+    names = pytest.driver.find_elements_by_css_selector('.card-deck .card-title')
+    descriptions = pytest.driver.find_elements_by_css_selector('.card-deck .card-text')
+
+    for i in range(len(names)):
+        assert images[i].get_attribute('src') != ''
+        assert names[i].text != ''
+        assert descriptions[i].text != ''
+        print (type(descriptions[i].text))
+        assert ',' in descriptions[i].text
+        parts = descriptions[i].text.split(",")
+        assert len(parts[0]) > 0
+        assert len(parts[1]) > 0
 
 def test_card_list_my_pets():
     """Проверка количества карточек притомцев, в списке: мои притомцы /my_pets, """
@@ -25,7 +65,15 @@ def test_card_list_my_pets():
 
     #Переходим на страницу /my_pets
     pytest.driver.find_element_by_css_selector('[href="/my_pets"]').click()
-    time.sleep(2)#всё происходит слишком быстро, потому добавил не много времени ожидания
+
+    """Добавляем явное ожидание загрузки таблицы с питомцами на странице  /my_pets"""
+    try:
+        element_table_wait = WebDriverWait(pytest.driver, 10).until(
+            EC.presence_of_element_located((By.ID, "all_my_pets")))  # Явное ожидание
+        element_table_wait
+    except TimeoutException:
+        print ("время ожидания вышло")
+        pytest.driver.quit()
 
     #Запрашиваем статистику пользователя, в которой содержится кол-во питомцев
     amount_my_pets = pytest.driver.find_element_by_xpath('//div[@class=".col-sm-4 left"]')
@@ -57,7 +105,7 @@ def test_card_list_my_pets():
     # Сравниваем длинну списка элементов и количество питомцев из карточки пользователя amount_my_pets
     assert len(list_my_pets) == int(amount_my_pets.text.split()[2])
 
-    #assert len(image_list) == len(set(image_list))  # Проверяем повторяющиеся фото
+    assert len(image_list) == len(set(image_list))  # Проверяем повторяющиеся фото
     assert int(amount_my_pets.text.split()[2]) > len(
         image_list) / 2  # Проверяем что хотя бы у половины питомцев есть фото
     assert int(amount_my_pets.text.split()[2]) == len(
